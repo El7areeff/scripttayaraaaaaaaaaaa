1601f89d3d236218f41696bb455d636662585ca5
El7areeff <m56793556@gmail.com>
2026-05-31 01:34:00 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-slate-100 font-bold bg-slate-950">
+      <span>بوابة اسكريبت الطيارة - الحريف</span>
+    </div>
+  );
+}
